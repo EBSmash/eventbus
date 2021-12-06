a7719a1e0829fee24e14f53ec070c08899d9ebc4
bush-did-711 <39170869+bush-did-711@users.noreply.github.com>
2021-12-06 00:35:23 -0800

    add test files

---
+++ src/test/java/Main.java
@@ -0,0 +1,46 @@
+import me.bush.eventbus.bus.EventBus;
+import me.bush.eventbus.handler.handlers.ReflectHandler;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Started: 12/2/2021
+ *
+ * @author bush
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        EventBus eventBus = new EventBus();
+        TestClass testClass = new TestClass();
+        TestEvent event = new TestEvent(":)");
+
+        // Post event before subscribing (does nothing)
+        eventBus.post(event);
+
+        eventBus.subscribe(testClass);
+        System.out.println("Subscribed object.");
+
+        // Post event (only object is subscribed, so only instance methods will be invoked)
+        eventBus.post(event);
+
+        eventBus.subscribe(TestClass.class);
+        System.out.println("Subscribed class.");
+
+        // Post event (class is subscribed, so static methods will be invoked too)
+        eventBus.post(event);
+
+        event.setCancelled(true);
+        System.out.println("Cancelled event.");
+
+        // Post cancelled event (only "RecieveCancelled" should be invoked)
+        eventBus.post(event);
+
+        eventBus.getInfo();
+
+        eventBus.setHandlerType(ReflectHandler.class);
+        System.out.println("Changed listener type.");
+
+        eventBus.getInfo();
+    }
+}
